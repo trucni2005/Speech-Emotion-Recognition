6ddaa1d7d4f477424773918c6dd248744263bbda
unknown <dangthitrucny@gmail.com>
2024-05-23 06:41:16 +0700

    cmt code

--- src/data_processing/english_data_processing.py
+++ src/data_processing/english_data_processing.py
@@ -5,6 +5,8 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from collections import defaultdict
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, MaxAbsScaler, RobustScaler, Normalizer
+from loguru import logger
 
 class ModelEvaluator:
     def __init__(self, models):
@@ -27,13 +29,17 @@ class ModelEvaluator:
 
         return y_pred
 
-    def _cross_validate(self, model, X, y):
-        kf = KFold(n_splits=10, shuffle=False)
+    def _cross_validate(self, model, X, y, scaler=None):
+        kf = KFold(n_splits=10, shuffle=True, random_state=42)
         acc_scores = []
 
         for train_index, test_index in kf.split(X):
             X_train, X_test = X.iloc[train_index], X.iloc[test_index]
             y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+            if scaler:
+                X_train = scaler.fit_transform(X_train)
+                X_test = scaler.transform(X_test)
 
             y_pred = self._train_and_predict(model, X_train, y_train, X_test)
 
@@ -42,37 +48,39 @@ class ModelEvaluator:
 
         avg_acc = np.mean(acc_scores)
         return avg_acc
-    
-    def _clean_data(self, df):
-        # Kiểm tra và thay thế các giá trị vô cùng bằng NaN
-        df.replace([np.inf, -np.inf], np.nan, inplace=True)
-        
-        # In ra số lượng giá trị NaN trước khi loại bỏ
-        print("Number of NaNs before drop:", df.isna().sum().sum())
-        
-        # Loại bỏ các hàng có giá trị NaN
-        df.dropna(inplace=True)
-        
-        # In ra số lượng hàng và cột sau khi làm sạch dữ liệu
-        print("Shape after cleaning:", df.shape)
-        
-        return df
-    
+
     def evaluate_models(self, data):
+        logger.info("Finding best model....")
         X, y = self._split_data(data)
-        X = self._clean_data(X)
         
         best_model = None
         best_avg_acc = 0.0
+        best_scaler = None
 
-        # Loop through models
-        for model in self.models:
-            avg_acc = self._cross_validate(model, X, y)
-            
-            # Lưu mô hình có độ chính xác trung bình cao nhất
-            if avg_acc > best_avg_acc:
-                best_avg_acc = avg_acc
-                best_model = model
+        scalers = [
+            ("None", None),
+            ("StandardScaler", StandardScaler()),
+            ("MinMaxScaler", MinMaxScaler()),
+            ("MaxAbsScaler", MaxAbsScaler()),
+            ("RobustScaler", RobustScaler()),
+            ("Normalizer", Normalizer())
+        ]
 
-        # Trả về mô hình có độ chính xác trung bình cao nhất
-        return best_model, best_avg_acc
+        results = defaultdict(list)
+
+        for scaler_name, scaler in scalers:
+            for model in self.models:
+                avg_acc = self._cross_validate(model, X, y, scaler)
+                logger.info(f'Model {model} - scaler: {scaler}, accuracy: {avg_acc}')
+                results[scaler_name].append((model, avg_acc))
+
+                if avg_acc > best_avg_acc:
+                    best_avg_acc = avg_acc
+                    best_model = model
+                    best_scaler = scaler_name
+
+        logger.info("Best Model: {}", best_model)
+        logger.info("Best Scaler: {}", best_scaler)
+        logger.info("Best Average Accuracy: {}", best_avg_acc)
+
+        return best_model, best_scaler, best_avg_acc
